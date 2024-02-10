210bb764b02ada59eef1c96253634428b4e2f29f
varunkukade <varunkukade999@gmail.com>
2024-02-10 23:47:23 +0530

    Library setup code added

--- android/app/src/main/java/com/videocomponent/MainActivity.kt
+++ android/app/src/main/java/com/videocomponent/MainActivity.kt
@@ -6,22 +6,26 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.content.Intent;
 import android.content.res.Configuration;
+import com.reactnativepipandroid.PipAndroidModule;
 
 class MainActivity : ReactActivity() {
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-      super.onConfigurationChanged(newConfig);
-      Intent intent = new Intent("onConfigurationChanged");
-      intent.putExtra("newConfig", newConfig);
-      this.sendBroadcast(intent);
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "VideoComponent"
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        val intent = Intent("onConfigurationChanged")
+        intent.putExtra("newConfig", newConfig)
+        sendBroadcast(intent)
+  }
+
+  override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean) {
+     PipAndroidModule.pipModeChanged(isInPictureInPictureMode)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
